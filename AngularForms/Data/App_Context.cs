2e1c84771069e17365a02f379c05365921900af0
Mina Ibrahem <Mina Ibrahem@MINAIBRAHEM>
2022-02-10 01:30:44 +0200

    Add project files.

---
+++ AngularForms/Data/App_Context.cs
@@ -0,0 +1,28 @@
+﻿using AngularForms.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularForms.Data
+{
+    public class App_Context :DbContext
+    {
+
+        public App_Context()
+        {
+        }
+
+        public App_Context(DbContextOptions<App_Context> options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=AngualrFormsDB;Trusted_Connection=True;");
+            }
+
+        }
+    }
+}
---
+++ AngularForms/Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularForms.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+            
+    }
+}
